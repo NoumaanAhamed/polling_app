d6a65b4faf6d1ccde108c8645ed78440cd3d8397
noumaan <noumaanahamed2@gmail.com>
2023-08-06 14:02:54 +0530

    finished basic polling app

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,50 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(req, question_id):
+    # return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=req.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            req,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
 def index(req):
@@ -30,9 +71,7 @@ def details(req, question_id):
 
 
 def results(req, question_id):
-    response = f"You're looking at the results of question {question_id}."
-    return HttpResponse(response)
-
-
-def vote(req, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    # response = f"You're looking at the results of question {question_id}."
+    # return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(req, "polls/results.html", {"question": question})
